1b2ccdb6937ac08130a5f93be31b0f85f95e43a3
Roman Kholoshenko <schneidersoft@ukr.net>
2015-12-10 19:09:21 +0200

    Intermediate commit #2 for works ver.

--- sat.py
+++ sat.py
@@ -1,9 +1,10 @@
 #!/bin/python
 
 import argparse
-from os import getpid, remove
+from shlex import split
+from os import getpid, remove, devnull
 from sys import argv
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, call, STDOUT
 
 def start_tune(args):
     
@@ -22,30 +23,34 @@ def start_tune(args):
     
     remove(tmpFile)
     
-    vlc_cmd = "vlc -v --color --ttl 12  --ts-es-id-pid"
-    if args.daemon == "yes":
-        vlc_cmd = vlc_cmd + " -d "
+    vlc_cmd = "/usr/bin/vlc -v --color --ttl 12  --ts-es-id-pid "
+    # if args.daemon == "yes":
+    #     vlc_cmd = vlc_cmd + " -d "
         
     if len(channels) > 0:
         
         vlc_cmd = vlc_cmd + "dvb-s:// --dvb-budget-mode --dvb-frequency={0}000 --dvb-srate={1}000 --dvb-polarization={2} --sout-standard-access=http --sout-mux-caching=5000 --sout-standard-mux=ts --sout '#duplicate".format(args.frequency, args.symbolRate, args.polarization) + "{"
         streams = list()
         info = list()
-        sout = list()
+        streams = list()
         d_port = args.port - 1
         for i in channels:
             d_port += 1
-            sout.append("dst=std{dst="+args.addr+":"+str(d_port)+"},select=\"program="+i[-1].replace("\n", "")+"\", ")
+            streams.append("dst=std{dst="+args.addr+":"+str(d_port)+"},select=\"program="+i[-1].replace("\n", "")+"\", ")
             info.append("{0:<20} - http://{1}:{2}".format(i[0],args.addr, d_port))
         
-        sout[-1] = sout[-1][0:(len(sout[-1]) - 2)]
+        streams[-1] = streams[-1][0:(len(streams[-1]) - 2)]
         
-        for i in sout:
+        for i in streams:
             vlc_cmd = vlc_cmd + i + " "
         vlc_cmd = vlc_cmd + "}'"
         
-    for i in info:
-        print(i)
+        print(vlc_cmd)
+        for i in info:
+            print(i)
+            
+        FNULL = open(devnull, 'w')
+        retcode = call(split(vlc_cmd), stdout=FNULL, stderr=STDOUT)
     
 def parse_args():
     parser = argparse.ArgumentParser(description='Process argumants')
@@ -55,7 +60,7 @@ def parse_args():
     parser.add_argument("-F", "--FEC", type=str, dest = "fec", default = "3/4", help = "specifies the code-rate to use for Forward Error Correction, like 2/3, 3/4, 5/6")
     parser.add_argument("-a", "--adapter", type=str, dest = "N", default = "0", help = "use DVB /dev/dvb/adapterN/")
     parser.add_argument("-H", "--adddress", type=str, dest = "addr", default = "0.0.0.0", help = "IP-address for listening sockets, by default = 0.0.0.0")
-    parser.add_argument("-D", "--daemon", type=str, dest = "daemon", default = "yes", help = "Run vlc as daemon")
+    # parser.add_argument("-D", "--daemon", type=str, dest = "daemon", default = "yes", help = "Run vlc as daemon")
     parser.add_argument("-P", "--start-port", type=int, dest = "port", default = 8000, help = "Start port for sockets")
     parser.set_defaults(func=start_tune)
     
